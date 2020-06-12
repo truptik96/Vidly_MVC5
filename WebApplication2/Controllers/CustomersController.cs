5c78d83d8867ce029a519af8ddc933700b4b0101
Elpro 2 <dhanashrigharge.elpro@gmail.com>
2020-06-12 15:37:23 -0700

    Add project files.

---
+++ WebApplication2/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customer = GetCustomers();
+            return View(customer);
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id=1,Name="John Smith" },
+                new Customer {Id=2,Name="Harry Williams" }
+            };
+        }
+
+        public ActionResult Details(int id)
+        {
+            var Customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (Customer == null)
+                return HttpNotFound();
+
+            return View(Customer);
+        }
+
+    }
+}
---
+++ WebApplication2/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+
+namespace WebApplication2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movie = GetMovies();
+            return View(movie);
+        }
+       
+        public IEnumerable<Movies> GetMovies()
+        {
+            return new List<Movies>
+            {
+                new Models.Movies {Id=1,Name="giugbuibhinioo" },
+                new Movies {Id=2,Name="kkkkkkkkkkkkkkk" }
+                };
+        }
+
+    }
+}
